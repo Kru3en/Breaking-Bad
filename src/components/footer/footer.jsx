f05b9317c13ae795efcf3bac1a1570fe7656f2a3
Игорь Городничук <gorodni4uk.igor@mail.ru>
2024-11-26 02:36:35 +0300

    Feat: end task 10

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,9 @@
 import styles from './footer.module.scss'
 export const Footer = () => {
-	return <div className={styles.containerPage}></div>
+	return (
+		<div className={styles.containerPage}>
+			<p>2024</p>
+			<p>@undenfined</p>
+		</div>
+	)
 }
