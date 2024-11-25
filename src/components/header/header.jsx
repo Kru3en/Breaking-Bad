0cbf9efe5531109e5cf144f1141e1deae26fc376
Игорь Городничук <gorodni4uk.igor@mail.ru>
2024-11-26 02:31:19 +0300

    Feat: end task 9. Делаю не по макету т.к. не нашел

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router-dom'
 import styles from './header.module.scss'
-
+import logo from './logo.png'
 export const Header = () => {
 	return (
 		<header className={styles.headerContainer}>
 			<nav className={styles.navBar}>
+				<img src={logo} alt='' className={styles.logo}/>
 				<ul className={styles.navList}>
 					<li className={styles.navItem}>
 						<NavLink
@@ -26,17 +27,8 @@ export const Header = () => {
 							Карточки
 						</NavLink>
 					</li>
-					<li className={styles.navItem}>
-						<NavLink
-							to='/Personage'
-							className={({ isActive }) =>
-								isActive ? styles.activeLink : styles.navLink
-							}
-						>
-							Персонажи
-						</NavLink>
-					</li>
 				</ul>
+				<span></span>
 			</nav>
 		</header>
 	)
