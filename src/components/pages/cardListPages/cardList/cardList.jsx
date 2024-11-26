a0d076a400fd5ebc99dad3c80a402d7166cfa4ff
Игорь Городничук <gorodni4uk.igor@mail.ru>
2024-11-26 22:46:44 +0300

    Feat: end task 12

--- src/components/pages/cardListPages/cardList/cardList.jsx
+++ src/components/pages/cardListPages/cardList/cardList.jsx
@@ -1,17 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchPersons } from '../../../../store/actions/person.js'
-import { addPerson } from '../../../../store/reducers/persons.js'
 import { Card } from './card/card.jsx'
+import { ListItem } from './listItem/listItem.jsx'
 import styles from './cardList.module.scss'
 
 export const CardList = () => {
 	const dispatch = useDispatch()
 	const persons = useSelector(state => state.persons)
-
-	const add = (name, birthday, img, status) => {
-		dispatch(addPerson({ name, birthday, img, status }))
-	}
+	const [isCardView, setIsCardView] = useState(true) 
 
 	useEffect(() => {
 		dispatch(fetchPersons())
@@ -20,12 +17,29 @@ export const CardList = () => {
 	if (!persons || persons.length === 0) {
 		return <div className={styles.loading}>Loading...</div>
 	}
-
+	const handleToggle = () => {
+		setIsCardView(!isCardView) 
+	}
 	return (
-		<div className={styles.contCardList}>
-			{persons.map(cardItem => (
-				<Card key={cardItem.id} {...cardItem} add={add} />
-			))}
+		<div className={styles.wrapPages}>
+			<div className={styles.toggleContainer}>
+				<label className={styles.switch}>
+					<input
+						type="checkbox"
+						checked={isCardView}
+						onChange={handleToggle}
+					/>
+					<span className={styles.slider}></span>
+				</label>
+			</div>
+
+			<div className={isCardView ? styles.contCardList : styles.contList}>
+				{isCardView
+					? persons.map(cardItem => <Card key={cardItem.name} {...cardItem} />)
+					: persons.map(listItem => (
+							<ListItem key={listItem.name} {...listItem} />
+					  ))}
+			</div>
 		</div>
 	)
 }
